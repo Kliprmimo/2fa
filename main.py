2cc6c0eef6ae9e4d94f27f7f36d68ab2640ca934
anon <grzes.dziechciarz@gmail.com>
2024-10-10 14:50:06 +0200

    working 2fa

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 import bcrypt
+import secrets
 import base64
 import json
 from getpass import getpass
+import smtplib
+import ssl
+import os
 '''
 passwords are stored as base64 representation of bcrypt hash
 '''
+
+
+def send_mail(address: str, code: int):
+    context = ssl.create_default_context()
+    sender_email = "2fa.securetransactions@gmail.com"
+    password = os.environ.get("EMAIL_PASSWORD")
+    port = 465
+    smtp_server = "smtp.gmail.com"
+    message = f"""\
+Subject: your 2fa code
+
+             {str(code)}"""
+
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, address, message)
 
 
 def generate_hash(password: str):
@@ -23,10 +43,11 @@ def check_hashe_pass(password: str, password_hash: bytes, password_salt: bytes):
 
 
 class user:
-    def __init__(self, username: str, password_hash: str, password_salt: str):
+    def __init__(self, username: str, password_hash: str, password_salt: str, email: str):
         self.username = username
         self.password_hash = password_hash
         self.password_salt = password_salt
+        self.email = email
 
 
 class usersData:
@@ -41,12 +62,12 @@ class usersData:
             index += 1
         return -1
 
-    def register(self, username: str, password: str):
+    def register(self, username: str, password: str, email: str):
         if self.check_if_username_exists(username) != -1:
             print('user with this username already exists, try different one')
             return False
         hash, salt = generate_hash(password)
-        new_user = user(username, base64.b64encode(hash).decode(), base64.b64encode(salt).decode())
+        new_user = user(username, base64.b64encode(hash).decode(), base64.b64encode(salt).decode(), email)
         self.users.append(new_user)
         return True
 
@@ -64,7 +85,7 @@ def open_db(filename: str):
     data = data['user_data']
     db = usersData([])
     for account in data:
-        new_user = user(account['username'], account['password_hash'], account['password_salt'])
+        new_user = user(account['username'], account['password_hash'], account['password_salt'], account['email'])
         db.users.append(new_user)
     return db
 
@@ -81,19 +102,27 @@ def save_db(filename: str, db: usersData):
 if __name__ == '__main__':
     db = open_db('test_db.json')
     while True:
-        action = input('login/register/save/exit\n>').strip()
+        action = input('login/register/save/exit\n').strip()
         if action == 'login':
             login = input('username: ').strip()
             password = getpass('password: ').strip()
-            if db.login(login, password):
-                print(f'Successfully logged in as {login}!')
-            else:
+            if not db.login(login, password):
                 print('login or username incorrect')
+            else:
+                code = secrets.randbelow(1000000)
+                send_mail(db.users[db.check_if_username_exists(login)].email, code)
+                code = input('secret from your mail: ').strip()
+                if code == str(code):
+                    print(f'Successfully logged in as {login}!')
+                else:
+                    print('incorrect code')
+
         elif action == 'register':
             login = input('username: ').strip()
             password = getpass('password: ').strip()
+            email = input('email: ').strip()
             print(password)
-            if db.register(login, password):
+            if db.register(login, password, email):
                 print(f'Successfully registered in as {login}!')
             else:
                 print('login or username incorrect')
